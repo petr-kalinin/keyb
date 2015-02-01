e2c2ee71186e3441b4d8f483c15a2587dee4c80c
unknown <temp110@ws-2k-114-09n.ccam.cmc.unn.net>
2015-02-01 15:39:18 +0300

    Added splitting by \r to allow running on Windows

--- keyb.js
+++ keyb.js
@@ -246,7 +246,7 @@ function startWord() {
 function loadDict(callback) {
     $.when(
         $.get("dictionaries/" + fName, function(data) {
-            samples = data.split("\n");
+            samples = data.split("\r\n");
         })
     ).done( function(x) {
         for (i=0; i<samples.length; i++)
